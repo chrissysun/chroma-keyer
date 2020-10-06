5e5812bfb8b0296ef2efa33ec46b4d562540d57f
Christine <christinesunnjm@gmail.com>
2020-10-05 23:33:11 -0400

    finish loading + enable user controls

--- app.js
+++ app.js
@@ -15,11 +15,21 @@ let processor = {
         }, 0);
     },
 
+    // load video
     doLoad: function() {
         this.video = document.querySelector('#video');
         this.c1 = document.querySelector('#c1');
         this.ctx1 = this.c1.getContext('2d');
         this.c2 = document.querySelector('#c2');
         this.ctx2 = this.c2.getContext('2d');
+        let self = this;
+        this.video.addEventListener('play', function() {
+            self.width = self.video.videoWidth;
+            self.height = self.video.videoHeight;
+            self.timerCallback();
+        });
+    },
+    computeFrame: function() {
+        this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
     }
 };
