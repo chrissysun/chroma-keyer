9ead6453ef7ab826de1a3ab50d11e180c30d65e5
Christine <christinesunnjm@gmail.com>
2020-10-05 23:55:32 -0400

    implemented green screen masking algorithm

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ let processor = {
 
     // load video
     doLoad: function() {
-        this.video = document.querySelector('#video');
-        this.c1 = document.querySelector('#c1');
-        this.ctx1 = this.c1.getContext('2d');
-        this.c2 = document.querySelector('#c2');
-        this.ctx2 = this.c2.getContext('2d');
+        this.video = document.querySelector("#video");
+        this.c1 = document.querySelector("#c1");
+        this.ctx1 = this.c1.getContext("2d");
+        this.c2 = document.querySelector("#c2");
+        this.ctx2 = this.c2.getContext("2d");
         let self = this;
-        this.video.addEventListener('play', function() {
+        this.video.addEventListener("play", function() {
             self.width = self.video.videoWidth;
             self.height = self.video.videoHeight;
             self.timerCallback();
@@ -31,5 +31,21 @@ let processor = {
     },
     computeFrame: function() {
         this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
+
+        // get each frame from video
+        let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
+        let l = frame.data.length / 4;
+        for (let i = 0; i < l; i++) {
+            let r = frame.data[i * 4]; // red
+            let g = frame.data[i * 4 + 1]; // green
+            let b = frame.data[i * 4 + 2]; // blue
+            if (g > 100 && r < 40) frame.data[i * 4 + 3] = 0;
+        }
+        this.ctx2.putImageData(frame, 0, 0);
+        return;
     }
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+    processor.doLoad();
+});
